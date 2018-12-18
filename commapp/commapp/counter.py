9442dd4348f07e8bd701a783ddd3db1028a4f98e
Dmitry Myasnikov <saver_is_not@bk.ru>
2018-12-18 23:02:45 +0300

    Update counter constructor

--- commapp/commapp/counter.py
+++ commapp/commapp/counter.py
@@ -1,9 +1,12 @@
 class Counter(object):
-    def __init__(self):
-        self.name = "My Counter"
-        self.id = "00000"
-        self.info = ""
-        self.previous_value = -1
-        self.current_value = -1
-
-
+    def __init__(self,
+                 name="My Counter",
+                 id="00000",
+                 info="",
+                 previous_value=-1,
+                 current_value=-1):
+        self.name = name
+        self.id = id
+        self.info = info
+        self.previous_value = previous_value
+        self.current_value = current_value
