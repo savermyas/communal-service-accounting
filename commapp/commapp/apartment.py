66f76cd58ec23fa15874104a2de953b662b0bf70
Dmitry Myasnikov <saver_is_not@bk.ru>
2018-12-20 15:57:31 +0300

    Remove unused classes

--- commapp/commapp/apartment.py
+++
@@ -1,8 +0,0 @@
-class Apartment(object):
-    def __init__(self,
-                 name="",
-                 address="",
-                 service_providers=[]):
-        self.name = name
-        self.address = address
-        self.service_providers = service_providers
